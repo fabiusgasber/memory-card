a6af2b41a48e586053f4218b3472aac007ca76cf
Fabius Gasber <fabiusgasber@gmail.com>
2025-04-25 14:36:54 +0200

    feat: add app module

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,15 +1,15 @@
-import '../Stylesheets/App.css'
-import Scoreboard from './Scoreboard'
-import Gameboard from './Gameboard'
+import "../Stylesheets/App.css";
+import Scoreboard from "./Scoreboard";
+import Gameboard from "./Gameboard";
+import getNflTeams from "./fetch";
 
 function App() {
-
   return (
     <>
       <h1>Gridiron Memory</h1>
       <Gameboard />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
