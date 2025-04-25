c1f537747eff600be37de02394b68feaa8d9064a
Fabius Gasber <fabiusgasber@gmail.com>
2025-04-25 14:32:33 +0200

    feat: add gameboard component

--- src/Components/Gameboard.jsx
+++ src/Components/Gameboard.jsx
@@ -1,8 +1,40 @@
-const Gameboard = () => {
+import { useEffect } from "react";
+import { useState } from "react";
+import Cards from "./Cards";
+import "../Stylesheets/Gameboard.css"
+import { randomizeArray, useFetch } from "./fetch"
+import useScoreboard from "./Scoreboard"
 
-    return (
-        <></>
-    );
-}
+const Gameboard = () => {
+    
+   const { data, setData, reload, setReload } = useFetch();
+   const { score, highScore, setHighscore, resetScore, wonRound, isHighscore } = useScoreboard();
+   
+   const handleClick = () => {
+    setData(randomizeArray(data));
+    if(score + 1 < data.length) {
+        wonRound();
+    }
+    else if(score >= data.length - 1) {
+        if(isHighscore) {
+          setHighscore(score + 1);
+        }
+        resetScore();
+        setReload(!reload);
+    }
+   }
+
+  return (
+    <main>
+    <section>
+      {data ? (
+        data.map((teams) => <Cards key={teams.team.uid} url={teams.team.logos[0].href} name={teams.team.displayName} onClick={() => setData(randomizeArray(data))} />)
+      ) : (
+        <h1>...Loading</h1>
+      )}
+      </section>
+    </main>
+  );
+};
 
 export default Gameboard;
