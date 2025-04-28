d028c0d0e82e0f0bbd100ae935caace1391105f3
Fabius Gasber <fabiusgasber@gmail.com>
2025-04-28 16:42:10 +0200

    feat: add description

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -8,6 +8,7 @@ function App() {
         <h1>Gridiron Memory</h1>
       </header>
       <main>
+        <p>Get points by clicking on an image but don't click on any more than once!</p>
         <Gameboard />
       </main>
     </>
