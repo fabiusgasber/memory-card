125ca3e6fa31436f5b23342f816dd8b1e3e6f2de
Fabius Gasber <fabiusgasber@gmail.com>
2025-04-28 12:06:51 +0200

    feat(Gameboard, Cards): check if card has been selected before

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,6 +1,6 @@
-const Cards = ({ url, name, onClick }) => {
+const Cards = ({ url, name, onClick, uid }) => {
   return (
-    <figure onClick={() => onClick()}>
+    <figure onClick={(e) => onClick(e)} data-id={uid}>
       <img width="150px" height="auto" src={url}></img>
       <figcaption>{name}</figcaption>
     </figure>
--- src/Components/Gameboard.jsx
+++ src/Components/Gameboard.jsx
@@ -2,23 +2,43 @@ import Cards from "./Cards";
 import "../Stylesheets/Gameboard.css"
 import { randomizeArray, useFetch } from "./fetch"
 import useScoreboard from "./Scoreboard"
+import { useState } from "react";
 
 const Gameboard = () => {
     
    const { data, setData, reload, setReload } = useFetch();
    const { score, highScore, setHighscore, resetScore, wonRound, isHighscore } = useScoreboard();
-   
-   const handleClick = () => {
-    setData(randomizeArray(data));
-    if(score + 1 < data.length) {
-        wonRound();
+   const [selected, setSelected] = useState([]);
+
+   const wasSelected = (id) => selected.find(elem => elem === id);
+   const addSelected = (id) => setSelected((selected) => [...selected, id]);
+
+   const handleClick = (e) => {
+    const id = e.target.closest("figure").getAttribute("data-id");
+    if(wasSelected(id) && isHighscore()) {
+      setHighscore(score);
+      resetScore();
+      setReload(!reload);
+      setSelected([]);
     }
-    else if(score >= data.length - 1) {
-        if(isHighscore) {
-          setHighscore(score + 1);
-        }
-        resetScore();
-        setReload(!reload);
+    else if(wasSelected(id)){
+      resetScore();
+      setReload(!reload);
+      setSelected([]);
+    }
+    else if(!wasSelected(id)){
+      setData(randomizeArray(data));
+      if(score + 1 < data.length) {
+          wonRound();
+          addSelected(id);
+      }
+      else if(score >= data.length - 1) {
+          if(isHighscore()) {
+            setHighscore(score + 1);
+          }
+          resetScore();
+          setReload(!reload);
+      }  
     }
    }
 
@@ -30,7 +50,7 @@ const Gameboard = () => {
     </section>
     <section>
       {data ? (
-        data.map((teams) => <Cards key={teams.team.uid} url={teams.team.logos[0].href} name={teams.team.displayName} onClick={handleClick} />)
+        data.map((teams) => <Cards key={teams.team.uid} uid={teams.team.uid} url={teams.team.logos[0].href} name={teams.team.displayName} onClick={handleClick} />)
       ) : (
         <h1>...Loading</h1>
       )}
